a48b8a61046fe48d18e26eeb7395fab0c6a0a283
berkay-g <35933635+berkay-g@users.noreply.github.com>
2023-11-04 13:48:25 +0300

    Update Window.cpp

--- db/Window.cpp
+++ db/Window.cpp
@@ -90,6 +90,12 @@ LRESULT CALLBACK Window::WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM 
             SetTextColor((HDC)wParam, RGB(109, 194, 222));
             SetBkMode((HDC)wParam, TRANSPARENT);
             return GetClassLongPtr(hwnd, GCLP_HBRBACKGROUND);
+        case WM_SIZE:
+            if (pThis->ResizeCallback != NULL)
+            {
+                pThis->ResizeCallback(*pThis, hwnd, LOWORD(lParam), HIWORD(lParam));
+            }
+            return 0;
         case WM_GETMINMAXINFO:
         {
             LPMINMAXINFO lpMMI = (LPMINMAXINFO)lParam;
